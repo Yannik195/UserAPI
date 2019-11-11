338d012ff5007d12bb6cb6b66767ab179e6a61a5
Yannik195 <yannisim@gmail.com>
2019-11-11 01:24:32 +0100

    2nd Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+    // Query
+    const query = User.find(function (err, users) {
+
+        res.send(users);
+    });
+}),
+
+    app.listen(3000);
+
+
+mongoose.connect("mongodb://localhost:27017/usersMobileAppDB", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+const UserSchema = new mongoose.Schema({
+    name: String,
+    lastname: String
+})
+
+const User = mongoose.model("User", UserSchema);
+
+const user = new User({
+    name: "Yannik",
+    lastname: "Simon"
+})
+// user.save();
+
+
+
